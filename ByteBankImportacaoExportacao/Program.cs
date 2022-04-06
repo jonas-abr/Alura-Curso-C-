eb3e883abd92d6eeee3269f205d7924e5950e294
jonas-abr <jonasdesenvolvedorfront@gmail.com>
2022-04-05 21:09:49 -0300

    first commit

---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,41 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // IO = INPUT E OUTPUT 
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; // 1kb
+
+            var numeroDeBytesLidos = -1;
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach(var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
+    }
+} 
+ 
